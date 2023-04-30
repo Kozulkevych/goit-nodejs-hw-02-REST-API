d6ed4e6838a7a87f286c0f9d4f35bb1febfc6fe6
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-04-30 23:45:16 +0300

    add solution

---
+++ routes/api/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/auth");
+
+const { authenticate, validateBody } = require("../../middlewares");
+
+const { schemas } = require("../../database/models/user");
+
+const router = express.Router();
+// signup
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+// signin
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
+
+// router.patch("/:id/subscription", authenticate, ctrl.updateSubscription);
+
+module.exports = router;
