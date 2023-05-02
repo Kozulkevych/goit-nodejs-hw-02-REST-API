6a96d4405f1ca18708396ea9e39ad8dbafcce01a
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-05-02 14:25:41 +0300

    add fix

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,9 +1,9 @@
-const { Contact } = require("../database/models/contact");
+const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getContacts = async (req, res) => {
   const result = await Contact.find({}, "-createdAt -updateAt");
-  res.json(result);
+  res.status(200).json(result);
 };
 
 const getById = async (req, res) => {
@@ -12,10 +12,14 @@ const getById = async (req, res) => {
   if (!result) {
     throw HttpError(404, "Not found");
   }
-  res.json(result);
+  res.status(200).json(result);
 };
 
 const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    throw HttpError(400, "missing required name field");
+  }
   const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
@@ -26,31 +30,40 @@ const deleteContact = async (req, res) => {
   if (!result) {
     throw HttpError(404, "Not found");
   }
-  res.json({
-    message: "Delete success",
+  res.status(200).json({
+    message: "contact deleted",
   });
 };
 
 const updateContact = async (req, res) => {
   const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    throw HttpError(400, "missing fields");
+  }
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
   });
   if (!result) {
     throw HttpError(404, "Not found");
   }
-  res.json(result);
+  res.status(200).json(result);
 };
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params;
+  const { favorite } = req.body;
+
+  if (!favorite) {
+    throw HttpError(400, "missing field favorite");
+  }
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
   });
   if (!result) {
     throw HttpError(400, "missing field favorite");
   }
-  res.json(result);
+  res.status(200).json(result);
 };
 
 module.exports = {
